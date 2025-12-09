2a07a721ee31ad1c497982b414948b470ab1380c
Edgar <edgarbagagem@gmail.com>
2025-12-09 16:08:55 +0000

    feat: day 2 part 2

--- day2/solution.go
+++ day2/solution.go
@@ -15,11 +15,10 @@ func Solution(part int) error {
 	case 1:
 		return partOne()
 	case 2:
+		return partTwo()
 	default:
 		return fmt.Errorf("invalid part %d for day 2", part)
 	}
-
-	return nil
 }
 
 func partOne() error {
@@ -76,6 +75,74 @@ func partOne() error {
 		}
 	}
 
+	fmt.Println(sum)
+	return nil
+}
+
+func partTwo() error {
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run main.go <input_file>")
+	}
+	inputPath := os.Args[1]
+
+	data, err := utils.InputStrings(inputPath)
+	if err != nil {
+		log.Fatalf("error parsing data from input: %s", err)
+	}
+
+	if len(data) > 1 {
+		log.Fatal("Error, expecting only a single line")
+	}
+
+	line := strings.Split(data[0], ",")
+
+	sum := 0
+	for _, valueRange := range line {
+		pair := strings.Split(valueRange, "-")
+		lower, err := strconv.Atoi(pair[0])
+		if err != nil {
+			panic("error parsing lower bound")
+		}
+		upper, err := strconv.Atoi(pair[1])
+		if err != nil {
+			panic("error parsing upper bound")
+		}
+
+		for i := lower; i <= upper; i++ {
+			s := strconv.Itoa(i)
+			digitsCount := len(s)
+
+			if digitsCount == 1 {
+				continue
+			}
+
+			for chunkSize := 1; chunkSize <= digitsCount/2; chunkSize++ {
+				if digitsCount%chunkSize != 0 {
+					continue
+				}
+
+				chunkCount := digitsCount / chunkSize
+				if chunkCount < 2 {
+					continue
+				}
+
+				chunk := s[0:chunkSize]
+				invalid := true
+
+				for k := 1; k < chunkCount; k++ {
+					if s[k*chunkSize:(k+1)*chunkSize] != chunk {
+						invalid = false
+						break
+					}
+				}
+
+				if invalid {
+					sum += i
+					break
+				}
+			}
+		}
+	}
 	fmt.Println(sum)
 	return nil
 }
